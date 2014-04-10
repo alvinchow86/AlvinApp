4579452012f42f8d24aedca41690579d1bf816e1
Alvin Chow <alvin@doordash.com>
2014-04-09 18:15:20 -0700

    playing with simple network access

--- src/com/example/alvinapp/MainActivity.java
+++ src/com/example/alvinapp/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.alvinapp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
     public final static String EXTRA_MESSAGE = "com.example.alvinapp.MESSAGE";
@@ -34,13 +47,12 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
-        //return true;
+        // return true;
     }
 
     private void showAlert(String message) {
-        new AlertDialog.Builder(this)
-        .setTitle("Hey")
-        .setMessage(message).show();
+        new AlertDialog.Builder(this).setTitle("Hey").setMessage(message)
+                .show();
     }
 
     @Override
@@ -50,16 +62,17 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         switch (id) {
-            case R.id.action_search:
-                showAlert("search");
-                return true;
-            case R.id.action_settings:
-                showAlert("settings");
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        case R.id.action_search:
+            showAlert("search");
+            return true;
+        case R.id.action_settings:
+            showAlert("settings");
+            return true;
+
+        default:
+            return super.onOptionsItemSelected(item);
         }
-        //return super.onOptionsItemSelected(item);
+        // return super.onOptionsItemSelected(item);
     }
 
     /** Called when the user clicks the Send button */
@@ -68,8 +81,87 @@ public class MainActivity extends ActionBarActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
         intent.putExtra(EXTRA_MESSAGE, message);
         this.startActivity(intent);
+    }
+
+    public void networkTest(View view) {
+        // showAlert("algorithm");
+        String stringUrl = "http://doordash.ngrok.com/api/consumer/me";
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            showAlert("No network connection available.");
+        }
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            showAlert(result);
+        }
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the web page content as a InputStream, which it returns as
+    // a string.
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("AlvinApp", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException,
+            UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
     }
 
     /**
@@ -97,19 +189,17 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.list_fragment_main, container,
-                    false);
+            View rootView = inflater.inflate(R.layout.list_fragment_main,
+                    container, false);
 
-            String[] items = {"Engineering","Operations","Merchant"};
+            String[] items = { "Engineering", "Operations", "Merchant" };
 
             /*
-            this.setListAdapter(new ArrayAdapter<String>(this,
-                    android.R.layout.simple_list_item_1,
-                    items));
-            */
+             * this.setListAdapter(new ArrayAdapter<String>(this,
+             * android.R.layout.simple_list_item_1, items));
+             */
             return rootView;
         }
     }
-
 
 }
