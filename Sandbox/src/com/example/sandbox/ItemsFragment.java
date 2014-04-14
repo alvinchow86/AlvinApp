586d9eb6e02a0bc5b03f560140e8058c013cf48b
Alvin Chow <alvin@doordash.com>
2014-04-13 19:54:40 -0700

    start to do list fragment without listfragment

--- Sandbox/src/com/example/sandbox/ItemsFragment.java
+++ Sandbox/src/com/example/sandbox/ItemsFragment.java
@@ -2,20 +2,23 @@ package com.example.sandbox;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 
-public class ItemsFragment extends ListFragment {
-    /*
+public class ItemsFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_items, container, false);
+        return inflater.inflate(R.layout.fragment_list, container, false);
     }
-    */
+
     Callbacks mListener;
 
     @Override
@@ -24,8 +27,14 @@ public class ItemsFragment extends ListFragment {
 
         String[] items = {"Dispatch","Merchant","Consumer","Driver"};
 
-        setListAdapter(new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_activated_1, items));
+        ListView listView = (ListView) getView().findViewById(R.id.list);
+        ArrayAdapter listAdapter = new ArrayAdapter<String>(getActivity(),
+                     android.R.layout.simple_list_item_activated_1, items);
+        listView.setAdapter(listAdapter);
+
+
+        //setListAdapter(new ArrayAdapter<String>(getActivity(),
+         //       android.R.layout.simple_list_item_activated_1, items));
 
 
 
@@ -46,12 +55,19 @@ public class ItemsFragment extends ListFragment {
         }
     }
 
+    /*
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         //string stringId = Long.toString(id);
+        //ListAdapter adapter;
         System.out.println(id);
+
+        //adapter = getListAdapter();
+
+
         mListener.onItemSelected(id);
     }
+    */
 
 
 }
