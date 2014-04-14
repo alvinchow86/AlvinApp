bde430b5b84f5990110d22663b238f93e02da602
Alvin Chow <alvin@doordash.com>
2014-04-13 17:41:55 -0700

    mess around with list fragment

---
+++ Sandbox/src/com/example/sandbox/ItemsFragment.java
@@ -0,0 +1,30 @@
+package com.example.sandbox;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+
+public class ItemsFragment extends ListFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_items, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        String[] items = {"Dispatch","Merchant","Consumer","Driver"};
+
+        setListAdapter(new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_activated_1, items));
+
+
+    }
+
+}
