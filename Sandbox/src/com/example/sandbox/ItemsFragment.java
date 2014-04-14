95b71b119adc5a463031245c1897490a973fc0b4
Alvin Chow <alvin@doordash.com>
2014-04-13 17:54:09 -0700

    remove unnecessary list layout

--- Sandbox/src/com/example/sandbox/ItemsFragment.java
+++ Sandbox/src/com/example/sandbox/ItemsFragment.java
@@ -9,11 +9,13 @@ import android.widget.ArrayAdapter;
 
 
 public class ItemsFragment extends ListFragment {
+    /*
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_items, container, false);
     }
+    */
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
